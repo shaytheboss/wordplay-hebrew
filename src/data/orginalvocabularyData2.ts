c202485964478563580f6958d5d3f8a328f58cfc
shaytheboss <156617988+shaytheboss@users.noreply.github.com>
2025-03-22 08:57:22 +0200

    Update orginalvocabularyData2.ts

--- src/data/orginalvocabularyData2.ts
+++ src/data/orginalvocabularyData2.ts
@@ -1,1 +1,41 @@
 
+export interface VocabularyItem {
+  english: string;
+  hebrew: string;
+}
+
+export const vocabularyData: VocabularyItem[] = [
+  // Add more words here:
+  { english: "steal", hebrew: "לגנוב" },
+  { english: "finish", hebrew: "לסיים" },
+  { english: "run away", hebrew: "לברוח" },
+  { english: "make a call", hebrew: "להתקשר" },
+  { english: "arrive", hebrew: "להגיע" },
+  { english: "fill", hebrew: "למלא" },
+  { english: "return", hebrew: "לחזור/להחזיר" },
+  { english: "start", hebrew: "להתחיל" },
+  { english: "invent", hebrew: "להמציא" },
+  { english: "marry", hebrew: "להתחתן" },
+  { english: "visit", hebrew: "לבקר" },
+  { english: "date", hebrew: "תאריך" },
+  { english: "song", hebrew: "שיר" },
+  { english: "price", hebrew: "מחיר" },
+  { english: "medal", hebrew: "מדליה" },
+  { english: "guide", hebrew: "מדריך" },
+  { english: "bowl", hebrew: "קערה" },
+  { english: "furniture", hebrew: "רהיטים" },
+  { english: "smile", hebrew: "חיוך" },
+  { english: "competition", hebrew: "תחרות" },
+  { english: "factory", hebrew: "מפעל" },
+  { english: "entry", hebrew: "כניסה" },
+  { english: "hill", hebrew: "גבעה" },
+  { english: "expensive", hebrew: "יקר" },
+  { english: "smart", hebrew: "חכם" },
+  { english: "simple", hebrew: "פשוט" },
+  { english: "round", hebrew: "עגול" },
+  { english: "comfortable", hebrew: "נוח" },
+  { english: "flat", hebrew: "שטוח" },
+  { english: "cheap", hebrew: "זול" },
+  { english: "heavy", hebrew: "כבד" },
+  { english: "Car", hebrew: "מכונית" }
+];
